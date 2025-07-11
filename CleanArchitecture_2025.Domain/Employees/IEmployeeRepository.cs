9017cc838c306deeec8b302e7d91f401e3a72147
YasarDogann <yasardgn99@gmail.com>
2025-07-11 18:55:39 +0300

    Add project files.

---
+++ CleanArchitecture_2025.Domain/Employees/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture_2025.Domain.Employees
+{
+    // Ana işlemleri bu repository'de tutarım : Create, update, delete, get
+    public interface IEmployeeRepository : IRepository<Employee>
+    {
+    }
+}
